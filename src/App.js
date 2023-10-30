994ecce330bdb346e1ff25cf2fc179c04cd3f213
Rahulkotha1 <rahul.kotha27@gmail.com>
2023-10-30 22:48:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import TaskFour from './Components/TaskFour';
+
+function App() {
+  return (
+    <div className='bg-dark container-fluid'>
+      <TaskFour />
+    </div>
+  );
+}
+
+export default App;
